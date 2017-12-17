b059c68612af45097b1bb8f959014f3461b3b078
Pirmin Rehm <pr057@hdm-stuttgart.de>
2017-12-17 09:17:03 +0100

    refactor the old test

--- test/voting.js
+++ test/voting.js
@@ -1,21 +1,33 @@
 const Voting = artifacts.require("./Voting.sol");
 Web3 = require("web3");
 const web3 = new Web3();
+const expect = require('chai').expect;
 const data = require('./data.json');
 
-nr = (num) => Number(num.toString(10));
+
+let nr = num => Number(num.toString(10));
+let f8 = str => web3.fromUtf8(str);
+let t8 = str => web3.toUtf8(str);
 
 contract('Voting', (accounts) => {
 
   let contract;
-  beforeEach((done) => {
-    Voting.deployed().then(instance => {
-      contract = instance;
-      done();
-    });
-  });
+  // beforeEach((done) => {
+  //   Voting.deployed().then(instance => {
+  //     contract = instance;
+  //     done();
+  //   });
+  // });
 
-  describe('Unit Tests', () => {
+  describe('Init-Tests', () => {
+    beforeEach(done => {
+      Voting.new(f8('init_test_voting'))
+      .then(instance => {
+        contract = instance;
+        done();
+      });
+    });
+
     it("should not have inital any voting users", async () => {
       const count = await contract.getVotingUsersCount.call();
       assert.strictEqual(nr(count), 0, "VotingUsers count is not 0");
@@ -25,61 +37,110 @@ contract('Voting', (accounts) => {
       const count = await contract.getVotedLocationsCount.call();
       assert.strictEqual(nr(count), 0, "VotedLocations count is not 0");
     });
+    it("should not be possilbe to add negative points", async () => {
+      try {
+        const res = await contract.addVote(data.uri1, -1 ,{from: accounts[0]});
+        expect(res).to.be.null;
+      } catch(e) {
+        expect(e).to.be.an('error');
+      }
+    });
+
+    it("should not be possilbe to add more than 100 points in one vote", async () => {
+      try {
+        const res = await contract.addVote(data.uri1, 101 ,{from: accounts[0]});
+        expect(res).to.be.null;
+      } catch(e) {
+        expect(e).to.be.an('error');
+      }
+    });
+
+    it("should not be possilbe to exceed 100 points per user", async () => {
+      try {
+        const res0 = await contract.addVote(data.uri1, 100 ,{from: accounts[0]});
+        const res = await contract.addVote(data.uri1, 1 ,{from: accounts[0]});
+        expect(res).to.be.null;
+      } catch(e) {
+        expect(e).to.be.an('error');
+      }
+    });
   
+    it("should get the name of the vote", async () => {
+      const votingName = await contract.getVotingName.call();
+      expect(t8(votingName)).to.equal('init_test_voting');
+    });
+  });
+
+
+  describe('Story 1: Add Votes from different Accounts', () => {
+    before(done => {
+      Voting.new(f8('story_voting'))
+      .then(instance => {
+        contract = instance;
+        done();
+      });
+    });
+
     it("should add a new voting of 50 points for location_1 from acc_0", async () => {
       const res = await contract.addVote(data.uri1, 50 ,{from: accounts[0]});
-      assert.isString(res.tx,"no transaction hash was returned");
-      assert.isNumber(res.receipt.gasUsed, "used gas is not specified");
-      assert.isString(res.receipt.blockHash,"blockHash is not specified");
-      assert.isNumber(res.receipt.blockNumber, "blockNumber is not specified");
+      expect(res).not.to.be.null;
+      expect(res.receipt).to.be.an('object');
+      expect(res.receipt.blockNumber).to.be.gt(0);
+      expect(res.receipt.gasUsed).to.be.gt(0);
+      expect(res.receipt.transactionHash).to.be.a('string');
     });
   
     it("should now have exacly one voting users", async () => {
       const count = await contract.getVotingUsersCount.call();
-      assert.strictEqual(nr(count), 1, "VotingUsers count is not 1");
+      expect(nr(count)).to.equal(1);
     });
   
     it("should now have exacly one voted location", async () => {
       const count = await contract.getVotedLocationsCount.call();
-      assert.strictEqual(nr(count), 1, "VotedLocations count is not 1");
+      expect(nr(count)).to.equal(1);
+      
     });
   
-    it("should be possible to get location_1 points by uri", async () => {
+    it("should get location_1 points by uri", async () => {
       const res = await contract.getLocationPointsByURI.call(data.uri1);
-      assert.equal(web3.toUtf8(res[0]), data.uri1, "location uri is wrong");
-      assert.equal(nr(res[1]), 50, "location points are wrong");
+      expect(t8(res[0])).to.equal(data.uri1);
+      expect(nr(res[1])).to.equal(50);
     });
   
-    it("should be possible to get location_1 points by index ", async () => {
+    it("should get location_1 points by index ", async () => {
       const res = await contract.getLocationPointsByIndex.call(0);
-      assert.equal(web3.toUtf8(res[0]), data.uri1, "location uri is wrong");
-      assert.equal(nr(res[1]), 50, "location points are wrong");
+      expect(t8(res[0])).to.equal(data.uri1);
+      expect(nr(res[1])).to.equal(50);
     });
-  
-    it.skip("should not be possilbe to add negative points", function() {});
-    it.skip("should not be possilbe to add more than 100 points in one vote", function() {});
-    it.skip("should not be possilbe to exceed 100 points per user", function() {});
-  
-    it.skip("should be possible to get the name of the vote", function() {});
+
+    it("should add votes of 250 points for location_1 from different accounts", async () => {
+      var res;
+      res = await contract.addVote(data.uri1, 50, {from: accounts[0]});
+      expect(res).to.be.an('object');;
+      res = await contract.addVote(data.uri1, 50, {from: accounts[1]});
+      expect(res).to.be.an('object');;
+      res = await contract.addVote(data.uri1, 50, {from: accounts[2]});
+      expect(res).to.be.an('object');;
+      res = await contract.addVote(data.uri1, 50, {from: accounts[3]});
+      expect(res).to.be.an('object');;
+      res = await contract.addVote(data.uri1, 50, {from: accounts[4]});
+      expect(res).to.be.an('object');;
+    });
+
+    it("should get updated location_1 points", async () => {
+      const res = await contract.getLocationPointsByURI.call(data.uri1);
+      expect(t8(res[0])).to.equal(data.uri1);
+      expect(nr(res[1])).to.equal(300);
+    });
   })
 
-  describe('Story 1', () => {
-    it("should be possible to add votes for location_1 from different accounts", async () => {
-      const newContract = await Voting.new();
-      
-      await newContract.addVote(data.uri1, 50, {from: accounts[0]});
-      await newContract.addVote(data.uri1, 50, {from: accounts[1]});
-      await newContract.addVote(data.uri1, 50, {from: accounts[2]});
-      await newContract.addVote(data.uri1, 50, {from: accounts[3]});
-      await newContract.addVote(data.uri1, 50, {from: accounts[4]});
-
-      const res = await newContract.getLocationPointsByURI.call(data.uri1);
-      assert.equal(web3.toUtf8(res[0]), data.uri1, "location uri is wrong");
-      assert.equal(nr(res[1]), 250, "location points are wrong");
+  describe('Story 2: Simulate voting and close voting by 4 users', () => {
+    before(done => {
+      Voting.new(f8('story_voting'))
+      .then(instance => {
+        contract = instance;
+        done();
+      });
     });
-  });
-
-  describe('Story 2', () => {
-    it.skip("should be possible to add votes for different locations from one account", () => {});
   });
 });
