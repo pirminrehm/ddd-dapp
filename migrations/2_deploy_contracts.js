bba3d5017c7018c9b9f3125df16f6adbc9856819
Pascal Fecht <pf023@hdm-stuttgart.de>
2018-01-15 19:09:46 +0100

    Use deployed function to query contracts and generalize logging

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,11 +10,12 @@ var web3 = new Web3();
 
 
 module.exports = function(deployer) {
-  // deployer.deploy(Team).then(() => {
-  //   deployer.deploy(Location, Team.address);
-  //   deployer.deploy(Voting, web3.fromUtf8('auto_deployed_voting'));
-  // });
-  return deployer.deploy(Logging).then(() => {
-    console.log('***************** ' + Logging.address + ' *****************');
-  });
+  // Deploy default contracts so that we can use contract.deployed()
+  deployer.deploy(Team)
+    .then(() => deployer.deploy(Location, Team.address))
+    .then(() => deployer.deploy(
+      Voting, 
+      web3.fromUtf8('auto_deployed_voting'), 
+      Location.address
+    ));
 };
