ceaa9ba7cdc4d881ac3265f59251e55322c27971
Pascal Fecht <pf023@hdm-stuttgart.de>
2018-01-22 23:06:16 +0100

    Use truffle config in web3 provider to remove deuplicate configurations

--- truffle.js
+++ truffle.js
@@ -1,9 +1,9 @@
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
+      host: 'localhost', // Add your ip here to debug the app on your mobile device
       port: 7545,
-      network_id: '*' // Match any network id
+      network_id: 5777
     }
   }
 }
