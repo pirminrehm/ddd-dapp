69392d881d282935b747860d4133f90f847c23b3
Pirmin Rehm <pr057@hdm-stuttgart.de>
2018-01-26 20:36:33 +0100

    chart colors: change chart colors to 'Google Chart Color List'
    also changed colors of sliders

--- src/pages/voting-chart/voting-chart.ts
+++ src/pages/voting-chart/voting-chart.ts
@@ -8,7 +8,28 @@ import { Subject } from 'rxjs/Subject';
 declare var google:any;
 
 // TODO: More generic way to generate colors...
-export const SLIDE_COLORS = ['#488aff', '#7babff', '#005afa', '#aecbff'];
+export const PRIMARY_SLIDE_COLOR = '#630700';
+export const SLIDE_COLORS = [
+  '#3366CC',
+  '#DC3912',
+  '#FF9900',
+  '#109618',
+  '#990099',
+  '#3B3EAC',
+  '#0099C6',
+  '#DD4477',
+  '#66AA00',
+  '#B82E2E',
+  '#316395',
+  '#994499',
+  '#22AA99',
+  '#AAAA11',
+  '#6633CC',
+  '#E67300',
+  '#329262',
+  '#5574A6',
+  '#3B3EAC',
+];
 
 /**
  * Generated class for the VotingChartPage page.
@@ -42,7 +63,7 @@ export class VotingChartPage implements OnInit {
     legend: {position: 'none', textStyle: {color: 'white', fontSize: 15}},
     pieSliceText: 'value',
     pieSliceTextStyle: {color: 'white'}, 
-    colors: ['#f53d3d', ...SLIDE_COLORS]
+    colors: [PRIMARY_SLIDE_COLOR, ...SLIDE_COLORS]
   };
 
   constructor() {}
