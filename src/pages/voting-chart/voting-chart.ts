938779054ca2b8ac6c1f6d7a5af98f9e51c2fca8
Pascal Fecht <pf023@hdm-stuttgart.de>
2018-01-26 18:49:07 +0100

    Clear up the chart reload mechanism and draw chart after init view to avoid miss calculations

--- src/pages/voting-chart/voting-chart.ts
+++ src/pages/voting-chart/voting-chart.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewChild, ElementRef, Input, Output, EventEmitter, 
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/debounceTime';
+import { Subject } from 'rxjs/Subject';
 
 declare var google:any;
 
@@ -24,8 +25,10 @@ export class VotingChartPage implements OnInit {
   @ViewChild('pieChart') pieChart: ElementRef;
   @Input() locationPoints: LocationPoint[];
   @Input() displayLegend: Boolean;
-  @Input() reload: Observable<any>;
+  @Input() reload: Subject<any>;
   @Output() chartDrawn = new EventEmitter();
+
+  private ready$: Promise<any>; 
 
   totalPoints: number = 0;
   
@@ -47,29 +50,32 @@ export class VotingChartPage implements OnInit {
   ngOnInit() {
     if(this.displayLegend) {
       this.chartOptions.legend.position = 'right';
-      this.chartOptions.chartArea.right = 40;
-      this.chartOptions.chartArea.width = '80%';
+      this.chartOptions.chartArea.left = 20;
+      this.chartOptions.chartArea.right = 20;
+      this.chartOptions.chartArea.width = '100%';
     }
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(() => {
-      this.chart = new google.visualization.PieChart(this.pieChart.nativeElement);
-      this.drawChart();
+    this.ready$ = new Promise((resolve, reject) => {
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(() => {
+        this.chart = new google.visualization.PieChart(this.pieChart.nativeElement);
+        resolve();
+      });
     });
 
-    if(this.reload) {
-      this.reload.debounceTime(200).subscribe(_ => this.drawChart());
+    if(!this.reload) {
+      this.reload = new Subject();
     }
+    this.reload.debounceTime(200).subscribe(_ => this.drawChart());
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    const locationPoints = changes.locationPoints;
-    if(locationPoints && locationPoints.currentValue !== locationPoints.previousValue) {
-      this.drawChart();
-    }
+  ngAfterContentInit() {
+    this.reload.next(true);
   }
 
-  drawChart() {
-    if(!this.chart || !this.locationPoints) {
+  private async drawChart() {
+    await this.ready$;
+    
+    if(!this.locationPoints) {
       return;
     }
 
