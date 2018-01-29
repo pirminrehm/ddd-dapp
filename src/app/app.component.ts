ede63dfe27f2aa18fe7969ca897c489f09d96c3c
Pirmin Rehm <pr057@hdm-stuttgart.de>
2018-01-29 16:48:34 +0100

    add alert box for closing the app with the android backbutton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -11,11 +11,13 @@ import { MemberApprovedProvider } from './../providers/helpers/member-approved';
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, 
+  constructor(public platform: Platform, 
               statusBar: StatusBar, 
               splashScreen: SplashScreen,
+              public alertCtrl: AlertController,
               private memberApprovedProvider: MemberApprovedProvider) {
     
     platform.ready().then(() => {
@@ -23,7 +25,45 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.backgroundColorByHexString('#0e0705');
       splashScreen.hide();
+      this.initializeBackButtonAction();
       this.memberApprovedProvider.doCheck();
     });
   }
+
+
+  private initializeBackButtonAction() {
+    //android only
+    let lockPrompt = false;
+    this.platform.registerBackButtonAction(data => {
+      if (this.nav.canGoBack()) {
+        this.nav.pop();
+      } else if (!lockPrompt) {
+        lockPrompt = true;
+        let title = 'Close App';
+        let message = `Do you really want to close the app?`;
+        this.promptYesNoWithMessage(title,message).then(() => {
+          navigator['app'].exitApp()
+        }).catch(()=>{
+          lockPrompt = false;
+        })
+      }
+    }, 1)
+  }
+  
+  private promptYesNoWithMessage (title: string, message: string) {
+		return new Promise ((resolve,reject) => {
+			let buttons = [{ 
+        text: 'Yes',		handler: data => resolve(true) },{
+        text: 'No',	handler: data => reject({abort:true, message: null})
+      }];
+			let prompt = this.alertCtrl.create({
+				title: title,
+				message: message,
+        buttons: buttons,
+        cssClass: 'alert-dark'
+			});
+			prompt.present();
+			prompt.onDidDismiss(data => reject({abort:true, message: null}));
+		})
+	}
 }
